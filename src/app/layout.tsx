8d29efeb1e6cf192e1aea248ee9ce214d54a360c
zohaib ismail <Clamboy076@gmail.com>
2024-08-23 14:03:37 +0500

    issue solved

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,7 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import SessionWrapper from "../../components/sessionWrapper";
 
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -18,7 +16,7 @@ export default function RootLayout({
   return (
     <SessionWrapper>
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body>{children}</body>
     </html>
     </SessionWrapper>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import Dashboard from "../../components/Dashboard";
 const Login =()=> {
   return (
